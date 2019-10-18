53fa80a92a1da4433da1bdf7f9c644372a05eef1
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-18 18:04:26 -0400

    adds GoogleBook model

--- NYTApp/Models/GoogleBook.swift
+++ NYTApp/Models/GoogleBook.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let items: [Item]
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let volumeInfo: VolumeInfo
+}
+
+// MARK: - VolumeInfo
+struct VolumeInfo: Codable {
+    let title: String
+    let authors: [String]
+    let publisher: String
+    let publishedDate: String
+    let description: String
+    let pageCount: Int
+    let categories: [String]
+    let imageLinks: ImageLinks
+}
+
+// MARK: - ImageLinks
+struct ImageLinks: Codable {
+    let smallThumbnail, thumbnail: String
+}
