022a0aca3054f6e680b58eb2458a4233a0fbc3b3
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-18 17:10:13 -0400

    removes old secret file and gitignore, adds new empty Secrets file

---
+++ NYTApp/Confidential/Secrets.swift
@@ -0,0 +1,9 @@
+//
+//  Secrets.swift
+//  NYTApp
+//
+//  Created by Sunni Tang on 10/18/19.
+//  Copyright © 2019 Liana Norman, Sunni Tang, Adam Jackson, Malcolm Turnquest. All rights reserved.
+//
+
+import Foundation
