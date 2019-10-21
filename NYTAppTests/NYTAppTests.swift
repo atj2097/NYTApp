841763b25349980e228af294cb33e011f5fe1223
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-21 11:55:08 -0400

    adds testBestSellerModelData in test bundle

--- NYTAppTests/NYTAppTests.swift
+++ NYTAppTests/NYTAppTests.swift
@@ -19,16 +19,34 @@ class NYTAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testBestSellerModelData() {
+        // Arrange
+        guard let jsonPath = Bundle.main.path(forResource: "BestSellerJSON", ofType: "json") else {
+            XCTFail("Could not find BestSellerJSON file")
+            return
         }
+        
+        let jsonURL = URL(fileURLWithPath: jsonPath)
+        var bestSellerJSONData = Data()
+        
+        do {
+            bestSellerJSONData = try Data(contentsOf: jsonURL)
+        } catch {
+            XCTFail("\(error)")
+        }
+        
+        // Act
+        var bestSellers = [BestSeller]()
+        
+        do {
+            let bestSellersInfo = try BestSellerWrapper.decodeBestSellerFromData(from: bestSellerJSONData)
+            bestSellers = bestSellersInfo
+        } catch {
+            XCTFail("\(error)")
+        }
+        
+        // Assert
+        XCTAssertTrue(bestSellers.count == 15, "Was expecting 15 best sellers, but found \(bestSellers.count)")
     }
-
+            
 }
