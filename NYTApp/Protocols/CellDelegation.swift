13979282a1061d131b65e3c7fd80e814d4cbebb0
NormanLiana <liananorman@pursuit.org>
2019-10-21 14:45:47 -0400

    adds protocols folder with cell delegation swift file in folder

---
+++ NYTApp/Protocols/CellDelegation.swift
@@ -0,0 +1,9 @@
+//
+//  CellDelegation.swift
+//  NYTApp
+//
+//  Created by Liana Norman on 10/21/19.
+//  Copyright © 2019 Liana Norman, Sunni Tang, Adam Jackson, Malcolm Turnquest. All rights reserved.
+//
+
+import Foundation
