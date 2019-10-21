e4b3e9e29cae73e876813d801250e3d8affa6039
NormanLiana <liananorman@pursuit.org>
2019-10-21 10:32:29 -0400

    adds image view for book and sets constraints

--- NYTApp/Controllers/BestDVC.swift
+++ NYTApp/Controllers/BestDVC.swift
@@ -9,22 +9,35 @@
 import UIKit
 
 class BestDVC: UIViewController {
+    
+    // MARK: - UI Objects
+    lazy var bookImage: UIImageView = {
+        var book = UIImageView(frame: .zero)
+        book.image = #imageLiteral(resourceName: <#T##String#>)
+        return book
+    }()
 
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubViews()
+        setUpBookImage()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Contraint Methods
+    private func addSubViews() {
+        self.view.addSubview(bookImage)
+    }
+    
+    private func setUpBookImage() {
+        bookImage.translatesAutoresizingMaskIntoConstraints = false
+        [bookImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50),bookImage.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 20),bookImage.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20), bookImage.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.25)].forEach({$0.isActive = true})
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+// MARK: - Extensions
+
