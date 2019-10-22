a930aa07bd09ea282c9dbeee6d27cf70ac06ec21
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-22 15:37:08 -0400

    remove secrets

--- NYTApp/Confidential/Secrets.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Secrets.swift
-//  NYTApp
-//
-//  Created by Sunni Tang on 10/18/19.
-//  Copyright © 2019 Liana Norman, Sunni Tang, Adam Jackson, Malcolm Turnquest. All rights reserved.
-//
-
-import Foundation
