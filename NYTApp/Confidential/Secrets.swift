58c24fe63cc81ba18deec2979b5ad49540aa8781
Malcolm Turnquest <malcolmturnquest@pursuit.org>
2019-10-24 17:52:32 -0400

    Here you go

---
+++ NYTApp/Confidential/Secrets.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  NYTApp
+//
+//  Created by Malcolm Turnquest on 10/22/19.
+//  Copyright © 2019 Liana Norman, Sunni Tang, Adam Jackson, Malcolm Turnquest. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    static let sunniNYTAPIKey =  "JqrMIbNHAWTF1h0QsCTqHXfizLDSMZn6"
+    static let sunniGoogleAPIKey = "AIzaSyCchPWLHi19MWiJdrhmmhhZ1GiGLEH5zs8"
+}
